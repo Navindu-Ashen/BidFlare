85e2a9b27099026636a5951d6b52b5d7677d3e2d
Dulmin2021 <dulmin.edu@gmail.com>
2024-10-09 23:52:33 +0530

    Added countdown timer

--- BidFlareFrontend/src/Pages/Auction.jsx
+++ BidFlareFrontend/src/Pages/Auction.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import carOne from '../assets/images/car1.png'
 
 
+//This is the countdown
 function Auction(){
     const [bid,setBid] = useState('');
+    const [timeLeft, setTimeLeft] = useState({
+        days: 3,
+        Hours: 8,
+        Minutes: 0,
+        Seconds: 0,
+    });
+
+
+
     function handleBidSubmit(event){
         event.preventDefault();
         alert('Bid of $${bid} has been placed ');
@@ -13,6 +23,34 @@ function Auction(){
     function handleBidChange(event){
         setBid(event.target.value);
     }
+
+
+useEffect(() => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 3);
+    targetDate.setHours(targetDate.getHours() + 8);
+
+    const timer = setInterval(() => {
+        const now = new Date();
+        const difference= targetDate-now;
+
+        if(difference>0){
+            const days = Math.floor(difference / (1000*60*60*24));
+            const hours = Math.floor((difference % (1000*60*60*24))/ (1000*60*60));
+            const minutes = Math.floor((difference % (1000*60*60))/ (1000*60));
+            const seconds = Math.floor((difference % (1000*60)) / 1000);
+
+            setTimeLeft({days, hours, minutes, seconds});
+            
+        }else{
+            clearInterval(timer);
+            setTimeLeft({days: 0, hours: 0, minutes: 0, seconds: 0});
+        }
+
+    }, 1000);
+    return () => clearInterval(timer);
+}, []);
+
 
     return(
         <div className="bg-gray-100 min-h-screen p-4">
@@ -28,7 +66,7 @@ function Auction(){
                 {/**Bid Details */}
                 <div className='mb-6' >
                     <h2 className= "text-xl font-semibold" >Current Bid: $500,000</h2>
-                    <p className="text-gray-600" >Ends in: 3 Days 8 Hours</p>
+                    <p className="text-gray-600" >Ends in: {timeLeft.days} Days {timeLeft.hours}  Hours {timeLeft.minutes}  Minutes  {timeLeft.seconds} Seconds </p>
                     <p className="text-gray-600" >Minimum Increment: $50,000</p>
                 </div>
 
