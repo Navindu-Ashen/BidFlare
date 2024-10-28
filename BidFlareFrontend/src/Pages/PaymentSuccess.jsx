1b77d9c69d987d985713137d5cdbf3e65e8b7072
Dulmin2021 <dulmin.edu@gmail.com>
2024-10-28 22:43:42 +0530

    Changees some in Payment page

--- BidFlareFrontend/src/Pages/PaymentSuccess.jsx
+++ BidFlareFrontend/src/Pages/PaymentSuccess.jsx
@@ -5,16 +5,8 @@ function PaymentSuccess() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md text-center max-w-xs">
         <div className="flex items-center justify-center mb-4">
-          <div className="bg-green-100 rounded-full p-3">
-            <svg
-              className="w-8 h-8 text-green-500"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path d="M9 16.2l-3.5-3.5 1.4-1.4L9 13.4l7.1-7.1 1.4 1.4L9 16.2z" />
-            </svg>
-          </div>
+        <img
+            src="src/image/success.gif" alt="Successful " className="w-200 h-156"/>
         </div>
         <h2 className="text-2xl font-semibold text-gray-800">Payment Approved</h2>
         <p className="text-gray-600 mt-2">
@@ -22,9 +14,9 @@ function PaymentSuccess() {
         </p>
         <button
           className="mt-6 px-6 py-2 bg-green-500 text-white rounded-full hover:bg-green-600"
-          onClick={() => alert("Thank you for your payment!")}
         >
-          OK
+            <a href="/">
+          OK</a>
         </button>
       </div>
     </div>
